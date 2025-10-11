6622ef9259b0bf6d2012045d3b127d0d1eb750df
sundara26071978 <sunjsg@gmail.com>
2025-10-11 17:36:19 +0530

    hello world factory

---
+++ design_pattern_creation/helloworld_factory.py
@@ -0,0 +1,37 @@
+class FrenchLocalizer:
+    def __init__(self):
+        pass
+    def localize(self, msg):
+        translations = {
+            "car": "voiture",
+            "bike": "bicyclette",
+            "cycle": "cyclette"
+        }
+        return translations.get(msg, msg)
+class SpanishLocalizer:
+    def __init__(self):
+        pass
+    def localize(self, msg):    
+        translations = {
+            "car": "coche",
+            "bike": "bicicleta",
+            "cycle": "ciclo"    
+        }
+        return translations.get(msg, msg)
+class EnglishLocalizer: 
+    def localize(self, msg):
+        return msg
+def LocalizerFactory(language="English"):
+    localizers = {
+        "French": FrenchLocalizer,
+        "English": EnglishLocalizer,
+        "Spanish": SpanishLocalizer,
+    }
+    return localizers[language]()
+if __name__ == "__main__":
+    f = LocalizerFactory("French")
+    e = LocalizerFactory("English")
+    s = LocalizerFactory("Spanish")
+    message = ["car", "bike", "cycle"]
+    for msg in message:
+        print(f.localize(msg))
---
+++ design_pattern_creation/helloworld_without_factory.py
@@ -0,0 +1,36 @@
+class FrenchLocalizer:
+    def __init__(self):
+        pass
+    def localize(self, msg):
+        translations = {
+            "car": "voiture",
+            "bike": "bicyclette",
+            "cycle": "cyclette"
+        }
+        return translations.get(msg, msg)
+class SpanishLocalizer:
+    def __init__(self):
+        pass    
+    def localize(self, msg):
+        translations = {
+            "car": "coche",
+            "bike": "bicicleta",
+            "cycle": "ciclo"
+        }
+        return translations.get(msg, msg)
+class EnglishLocalizer:
+    def __init__(self):
+        pass
+    def localize(self, msg):
+        return msg
+    
+
+if __name__ == "__main__":
+    f = FrenchLocalizer()
+    e = EnglishLocalizer()
+    s = SpanishLocalizer()
+    message = ["car", "bike", "cycle"]
+    for msg in message:
+        print(f.localize(msg))
+        print(e.localize(msg))
+        print(s.localize(msg))
